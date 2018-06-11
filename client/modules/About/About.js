a49e457a4723ba464211ff5612a25c33d54c54cd
Pawel Z <t03c10@gmail.com>
2018-06-11 10:43:35 +0200

    initial commit

---
+++ client/modules/About/About.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+
+const About = () => {
+	return (
+		<div>
+			<h2>Zadanie 22.6</h2>
+			<a href="/">Back to main page</a>
+		</div>
+	)
+}
+
+export default About;
